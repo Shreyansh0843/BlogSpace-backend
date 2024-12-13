c0300c886c86d8888d41b7cd77e01fe654e0b5a7
Shreyansh Priyadarshi <shreyansh12361@gmail.com>
2024-12-14 01:25:10 +0530

    Removed the local host link

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://blogspace0843.netlify.app/'],
+    origin: ['https://blogspace0843.netlify.app/'],
     credentials: true
 }));
 app.use(express.json());
